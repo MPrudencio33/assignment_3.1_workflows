b9ffe36f0b5a74690b60be12cf2032e5e4055b8b
zanjero <zanjero3333@gmail.com>
2025-10-21 23:13:41 +0800

    add main terraform config

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ region = "us-east-1"
 }
 
 terraform { 
-backend "s3" { 
-bucket = "sctp-ce11-tfstate" 
-key    = "marlon.tfstate" 
-region = "us-east-1" 
-} 
+    backend "s3" { 
+    bucket = "sctp-ce11-tfstate" 
+    key    = "marlon.tfstate" 
+    region = "us-east-1" 
+    } 
 }
 
 resource "aws_s3_bucket" "app_bucket" {
