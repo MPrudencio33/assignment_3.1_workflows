7365ca84ee2a19dc20a001632736aa9f7eeb4e6b
zanjero <zanjero3333@gmail.com>
2025-10-25 02:42:33 +0800

    updates

--- main.tf
+++ main.tf
@@ -13,10 +13,3 @@ terraform {
 resource "aws_s3_bucket" "marlon-bucket" {
     bucket = "marlon-github-actions-bucket"
 }
-
-# An example resource that does nothing.
-#     resource "null_resource" "example" {
-#       triggers = {
-#         value = "A example resource that does nothing!"
-#       }
-#     }
