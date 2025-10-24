5dd9a2a3b8f302d7c66e5b0b232c1d9ad596ecc4
zanjero <zanjero3333@gmail.com>
2025-10-25 02:31:37 +0800

    updated yaml

--- main.tf
+++ main.tf
@@ -7,9 +7,16 @@ terraform {
         bucket = "sctp-core-tfstate" 
         key    = "marlon.tfstate" 
         region = "ap-southeast-1"
-    } 
+    }
 }
-
+    
 resource "aws_s3_bucket" "marlon-bucket" {
     bucket = "marlon-github-actions-bucket"
 }
+
+# An example resource that does nothing.
+#     resource "null_resource" "example" {
+#       triggers = {
+#         value = "A example resource that does nothing!"
+#       }
+#     }
