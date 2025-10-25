07d034337b9724eccbae328ff4a97f92e031ac7d
zanjero <zanjero3333@gmail.com>
2025-10-25 10:53:04 +0800

    updated the region to us-east-1

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
 provider "aws" { 
-    region = "ap-southeast-1"
+    region = "us-east-1"
 }
 
 terraform { 
     backend "s3" { 
         bucket = "sctp-core-tfstate" 
         key    = "marlon.tfstate" 
-        region = "ap-southeast-1"
+        region = "us-east-1"
     }
 }
     
 resource "aws_s3_bucket" "marlon-bucket" {
     bucket = "marlon-github-actions-bucket"
 }
-
 
 #resource "null_resource" "example" {
 #    triggers = {
